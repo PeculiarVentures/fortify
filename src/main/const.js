566ecd8a2e7360e541a511f1841441a537d0ba23
microshine <microshine@mail.ru>
2017-10-27 22:44:34 +0300

    Rename log file

--- src/main/const.js
+++ src/main/const.js
@@ -10,7 +10,7 @@ export const RESOURCES_DIR = path.join(SRC_DIR, 'resources');
 export const HTML_DIR = path.join(SRC_DIR, 'htmls');
 export const ICON_DIR = path.join(SRC_DIR, 'icons');
 
-export const APP_LOG_FILE = path.join(APP_TMP_DIR, `LOG.log`);
+export const APP_LOG_FILE = path.join(APP_TMP_DIR, `fortify.log`);
 export const APP_CONFIG_FILE = path.join(APP_TMP_DIR, `config.json`);
 export const APP_SSL_CERT_CA = path.join(APP_TMP_DIR, `ca.pem`);
 export const APP_SSL_CERT = path.join(APP_TMP_DIR, `cert.pem`);
