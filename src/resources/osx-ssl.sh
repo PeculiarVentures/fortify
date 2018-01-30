752b96570476d85ce902e9536e7fe50d6f0a532f
microshine <microshine@mail.ru>
2018-01-30 22:28:40 +0300

    Support sql: and dbm: version of NSS db for OSX

--- src/resources/osx-ssl.sh
+++ src/resources/osx-ssl.sh
@@ -18,12 +18,18 @@ then
     echo -e "Delete old cert"
     # Delete old cert
     # /usr/local/Cellar/nss/3.31/bin/certutil -D -n "${certificateName}" -d "${firefoxDefaultProfile}"
-    until ! ${CUR_DIR}/certutil -D -n "Fortify Local CA" -d "${firefoxDefaultProfile}"
+    until ! ${CUR_DIR}/certutil -D -n "Fortify Local CA" -d sql:"${firefoxDefaultProfile}"
     do
-        echo "Fortify certificate was removed"
+        echo "Fortify certificate was removed from sql:cert.db"
     done
-    ${CUR_DIR}/certutil -A -i "${certPath}" -n "${certificateName}" -t "C,c,c" -d "${firefoxDefaultProfile}"
-    ${CUR_DIR}/certutil -L -d "${firefoxDefaultProfile}"
+    until ! ${CUR_DIR}/certutil -D -n "Fortify Local CA" -d dbm:"${firefoxDefaultProfile}"
+    do
+        echo "Fortify certificate was removed from dbm:cert.db"
+    done
+    ${CUR_DIR}/certutil -A -i "${certPath}" -n "${certificateName}" -t "C,c,c" -d sql:"${firefoxDefaultProfile}"
+    ${CUR_DIR}/certutil -A -i "${certPath}" -n "${certificateName}" -t "C,c,c" -d dbm:"${firefoxDefaultProfile}"
+    ${CUR_DIR}/certutil -L -d sql:"${firefoxDefaultProfile}"
+    ${CUR_DIR}/certutil -L -d dbm:"${firefoxDefaultProfile}"
 
     sudo -u ${USER} bash ${CUR_DIR}/osx_firefox.sh
 fi
