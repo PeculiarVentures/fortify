e673b0626ef1c51e2a163e6a1224629149d2271b
donskov <donskovdmitriyv@gmail.com>
2020-04-28 19:10:34 +0300

    feat(about): Added container

---
+++ src/renderer/containers/about/index.tsx
@@ -0,0 +1,39 @@
+import * as path from 'path';
+import * as fs from 'fs';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import WindowProvider from '../../components/window_provider';
+import Container from './container';
+
+const PACKAGE_PATH = path.join(__dirname, '../../package.json');
+
+class Root extends WindowProvider<{}, {}> {
+  version: string;
+
+  constructor(props: {}) {
+    super(props);
+
+    this.version = Root.getVersion();
+  }
+
+  static getVersion() {
+    const json = fs.readFileSync(PACKAGE_PATH, { encoding: 'utf8' });
+    const data = JSON.parse(json);
+
+    return data.version;
+  }
+
+  render() {
+    return (
+      <Container
+        version={this.version}
+        onClose={this.close}
+      />
+    );
+  }
+}
+
+ReactDOM.render(
+  <Root />,
+  document.getElementById('root'),
+);
