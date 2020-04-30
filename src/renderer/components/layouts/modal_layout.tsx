dd0b62f4a92f93bef111da9c8a75401eb688ffb2
donskov <donskovdmitriyv@gmail.com>
2020-04-30 11:54:44 +0300

    feat(renderer): Added  modal layout

---
+++ src/renderer/components/layouts/modal_layout.tsx
@@ -0,0 +1,79 @@
+import * as React from 'react';
+import { Button, Typography } from 'lib-react-components';
+import { intl } from '../../../main/locale';
+
+const s = require('./styles/modal_layout.sass');
+
+export interface IModalLayoutProps {
+  children: React.ReactNode;
+  title: (string | React.ReactNode)[];
+  onReject: () => void;
+  onApprove: () => void;
+  textReject?: string;
+  textApprove?: string;
+}
+
+export default class ModalLayout extends React.Component<IModalLayoutProps> {
+  renderButtons() {
+    const {
+      onReject,
+      onApprove,
+      textReject,
+      textApprove,
+    } = this.props;
+
+    return (
+      <>
+        <Button
+          size="large"
+          className={s.button}
+          bgType="stroke"
+          color="grey_4"
+          textColor="black"
+          key="reject"
+          onClick={onReject}
+        >
+          {textReject || intl('cancel')}
+        </Button>
+        <Button
+          size="large"
+          className={s.button}
+          color="secondary"
+          key="approve"
+          onClick={onApprove}
+        >
+          {textApprove || intl('ok')}
+        </Button>
+      </>
+    );
+  }
+
+  render() {
+    const { children, title } = this.props;
+
+    return (
+      <section className={s.host}>
+        <div className={s.container_body}>
+          <div className={s.container_title}>
+            {title.map((value, index) => (
+              <Typography
+                type="b3"
+                align="auto"
+                key={index}
+                className={s.title}
+              >
+                {value}
+              </Typography>
+            ))}
+          </div>
+          <div className={s.container_content}>
+            {children}
+          </div>
+        </div>
+        <footer className={s.footer}>
+          {this.renderButtons()}
+        </footer>
+      </section>
+    );
+  }
+}
