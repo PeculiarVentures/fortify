56d795ba089598c8d66afb31953e790d1901c460
donskov <donskovdmitriyv@gmail.com>
2020-05-26 12:00:30 +0300

    feat(bundler): Added stencil bundler, updated dependencies

--- README.md
+++ README.md
@@ -1,18 +1,11 @@
-# Landing page for Fortify
+# `@peculiar/fortify-website
 
-#### Prepare
-- Clone latest version this repository
+### Development Workflow
+
 ```bash
+# Install npm dependencies
 npm install
-```
 
-#### Development
-```bash
-npm run start
-```
-Visit [http://localhost:3000](http://localhost:3000)
-
-#### Production
-```bash
-npm run build
-```
+# Run dev server
+npm start
+````
---
+++ stencil.config.ts
@@ -0,0 +1,27 @@
+import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
+
+// https://stenciljs.com/docs/config
+
+export const config: Config = {
+  globalStyle: 'src/global/app.scss',
+  taskQueue: 'async',
+  devServer: {
+    openBrowser: false,
+    port: 3000,
+  },
+  plugins: [
+    sass(),
+  ],
+  outputTargets: [
+    {
+      type: 'www',
+      serviceWorker: null,
+      copy: [
+        { src: 'pages/**/*.json' },
+        { src: 'assets', dest: 'assets' },
+        { src: 'manifest.json', dest: 'manifest.json' },
+      ],
+    },
+  ],
+};
