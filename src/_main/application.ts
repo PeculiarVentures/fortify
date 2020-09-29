2295ddb2233f44a9a3697e0d0758ea14b113ec26
donskov <donskovdmitriyv@gmail.com>
2020-09-29 14:02:02 +0300

    feat(main): add application

---
+++ src/_main/application.ts
@@ -0,0 +1,63 @@
+import { app } from 'electron';
+import { autoUpdater } from './updater';
+import { tray } from './tray';
+import { CHECK_UPDATE, CHECK_UPDATE_INTERVAL } from './constants';
+
+export class Application {
+  app = app;
+
+  public start() {
+    const gotTheLock = this.app.requestSingleInstanceLock();
+
+    if (!gotTheLock) {
+      this.app.quit();
+    }
+
+    if ('dock' in this.app) {
+      this.app.dock.hide();
+    }
+
+    this.onReady();
+  }
+
+  public exit() {
+    this.app.exit();
+  }
+
+  private async onReady() {
+    await this.app.whenReady();
+
+    tray.create();
+
+    await this.initAutoUpdater();
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  private async initAutoUpdater() {
+    if (CHECK_UPDATE) {
+      // TODO: Add handler.
+      autoUpdater.on('update-found', (version) => {
+        console.log('update-found', version);
+      });
+
+      // TODO: Add handler.
+      autoUpdater.on('update-not-found', () => {
+        console.log('update-not-found');
+      });
+
+      // TODO: Add handler.
+      autoUpdater.on('error', (error) => {
+        console.log('error', error);
+      });
+
+      await autoUpdater.checkForUpdates();
+
+      setInterval(
+        () => autoUpdater.checkForUpdates(),
+        CHECK_UPDATE_INTERVAL,
+      );
+    }
+  }
+}
+
+export const application = new Application();
---
+++ src/_main/index.ts
@@ -0,0 +1,3 @@
+import { application } from './application';
+
+application.start();
