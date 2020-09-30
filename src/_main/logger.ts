a8a9e2b7c57553865b615a4252016c85f22581b8
donskov <donskovdmitriyv@gmail.com>
2020-09-30 15:45:27 +0300

    feat(main): add logging switch

--- src/_main/logger.ts
+++ src/_main/logger.ts
@@ -1,3 +1,15 @@
 import * as winston from 'winston';
+import { APP_LOG_FILE } from './constants';
 
 export const logger = winston.createLogger();
+
+export const loggingSwitch = (enabled: boolean) => {
+  if (enabled) {
+    const options = { flag: 'w+' };
+
+    winston.add(new winston.transports.File({ filename: APP_LOG_FILE, options }));
+    winston.add(new winston.transports.Console());
+  } else {
+    winston.clear();
+  }
+};
