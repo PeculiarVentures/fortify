48a3fd01e5398289886d5e097a76a1c35745830e
donskov <donskovdmitriyv@gmail.com>
2020-10-29 18:06:10 +0200

    feat(constants): add mixpanel token

--- src/main/constants/index.ts
+++ src/main/constants/index.ts
@@ -1,4 +1,5 @@
 export * from './design';
 export * from './files';
+export * from './third_party';
 
 export const isDevelopment = process.env.NODE_ENV === 'development';
---
+++ src/main/constants/third_party.ts
@@ -0,0 +1,1 @@
+export const MIXPANEL_TOKEN = '31a2bd73f845a3bef59965badb55fe34';
