0f1824e28d7a1ae1ba2ab0950c71d3a72d4f53ad
donskov <donskovdmitriyv@gmail.com>
2021-02-05 17:58:16 +0200

    feat(main): add ip adress to each analytics event

--- src/main/logger/analytics.ts
+++ src/main/logger/analytics.ts
@@ -1,4 +1,5 @@
 import * as Mixpanel from 'mixpanel';
+import publicIp from 'public-ip';
 import { MIXPANEL_TOKEN } from '../constants';
 
 const mixpanel = Mixpanel.init(MIXPANEL_TOKEN);
@@ -14,13 +15,26 @@ export interface IAnalyticsOptions {
 export class Analytics {
   private userId: string;
 
+  private userIp!: string;
+
   constructor(options: IAnalyticsOptions) {
     this.userId = options.userId;
+
+    this.initIp();
+  }
+
+  private async initIp() {
+    try {
+      this.userIp = await publicIp.v4();
+    } catch {
+      //
+    }
   }
 
   event(category: string, action: string, params: Record<string, any>) {
     mixpanel.track(action, {
       distinct_id: this.userId,
+      ip: this.userIp,
       category,
       ...params,
     });
