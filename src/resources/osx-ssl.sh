cb1ecbb83a54fccc41ada5d160fcfac296327aa3
microshine <microshine@mail.ru>
2022-05-17 09:52:33 +0300

    refactor: remove odd code from osx-ssl.sh

--- src/resources/osx-ssl.sh
+++ src/resources/osx-ssl.sh
@@ -10,8 +10,3 @@ echo -e "certPath: ${certPath}"
 keychain=$(security default-keychain -d user | sed 's/"//g')
 security delete-certificate -c ${certificateName} ${keychain}
 security add-trusted-cert -r trustRoot -k ${keychain} ${certPath}
-certDir=$(dirname ${certPath})
-echo -e "certDir: ${certDir}"
-mkdir "$certDir/mkcert"
-cp ${certPath} "$certDir/mkcert/rootCA.pem"
-CAROOT="$certDir/mkcert" TRUST_STORES=system mkcert -install
