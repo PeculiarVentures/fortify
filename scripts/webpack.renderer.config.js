bccc47e153583250d63df0419bcbdab74e4cc6dd
donskov <donskovdmitriyv@gmail.com>
2023-09-19 17:44:19 +0300

    build: use only single application entry file

--- scripts/webpack.renderer.config.js
+++ scripts/webpack.renderer.config.js
@@ -5,10 +5,7 @@ const baseConfig = require('./webpack.base.config');
 module.exports = merge.smart(baseConfig, {
   target: 'electron-renderer',
   entry: {
-    message: path.join(__dirname, '../src/renderer/containers/message/index.tsx'),
-    'key-pin': path.join(__dirname, '../src/renderer/containers/key_pin/index.tsx'),
-    'p11-pin': path.join(__dirname, '../src/renderer/containers/p11_pin/index.tsx'),
-    preferences: path.join(__dirname, '../src/renderer/containers/preferences/index.tsx'),
+    index: path.join(__dirname, '../src/renderer/index.tsx'),
   },
   optimization: {
     splitChunks: {
