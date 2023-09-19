e1a2fddb149f0c35cd68ff08d6dd0022119bc657
donskov <donskovdmitriyv@gmail.com>
2023-09-19 17:42:25 +0300

    feat: add shared constants

---
+++ src/shared/constants/windows.ts
@@ -0,0 +1,13 @@
+export enum WindowsName {
+  Preferences = 'preferences',
+  Message = 'message',
+  KeyPin = 'key-pin',
+  P11Pin = 'p11-pin',
+}
+
+export enum WindowPreferencesName {
+  Sites = 'sites',
+  Settings = 'settings',
+  Updates = 'updates',
+  About = 'about',
+}
